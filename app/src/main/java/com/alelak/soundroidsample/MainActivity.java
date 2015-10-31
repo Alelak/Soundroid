f395dd4d98ecd48642c0d2f42bd491502bb6cfbd
unknown <antikanfous@gmail.com>
2015-10-31 13:28:34 -0400

    Modified demo app

--- app/src/main/java/com/alelak/soundroidsample/MainActivity.java
+++ app/src/main/java/com/alelak/soundroidsample/MainActivity.java
@@ -3,6 +3,7 @@ package com.alelak.soundroidsample;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -28,6 +29,7 @@ import retrofit.Retrofit;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getName();
     private RecyclerView recyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private SongAdapter songAdapter;
     private List<Track> tracks;
 
@@ -38,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupToolbar();
         setupRecyclerView();
+        setupSwipeRefreshLayout();
         getContent();
 
     }
@@ -47,6 +50,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    private void setupSwipeRefreshLayout() {
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getContent();
+            }
+        });
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(true);
+            }
+        });
+    }
 
     private void setupRecyclerView() {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
@@ -62,20 +81,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void getContent() {
-
         Call<List<Track>> call = Soundroid.getSoundcloudService().searchTracksByGenres("House", 20);
         call.enqueue(new Callback<List<Track>>() {
             @Override
             public void onResponse(Response<List<Track>> response, Retrofit retrofit) {
+                swipeRefreshLayout.setRefreshing(false);
                 if (response.isSuccess()) {
+                    tracks.clear();
                     tracks.addAll(response.body());
                     songAdapter.notifyDataSetChanged();
+
                 }
 
             }
 
             @Override
             public void onFailure(Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
